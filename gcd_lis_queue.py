9d1a8aa9e059b07b2f009734360d98de72fff419
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-02-14 22:18:34 +0530

    Update gcd_lis_queue.py

--- gcd_lis_queue.py
+++ gcd_lis_queue.py
@@ -15,10 +15,15 @@ def factor(lis):
         for j in range(miner,0,-1):
             if(divisible(lis,j)):
                 return j
+s =""
 n,q = map(int,input().split())
 temp = list(map(int,input().split()))
 for i in range(q):
     t,u = map(int,input().split())
     x = min(t,u)
     y = max(t,u)
-    print(factor(temp[x-1:y]),"\n")
+    if(i == 0):
+        s =str(factor(temp[x-1:y]))
+    else:
+        s = s+"\n"+str(factor(temp[x-1:y]))
+print(s)
