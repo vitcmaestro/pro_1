d943dca2f1ad1ed6694b730265ad7d069c5eb60e
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-02-14 22:53:50 +0530

    Create kolappam.py

---
+++ kolappam.py
@@ -0,0 +1,10 @@
+n = int(input(""))
+li = list(map(int,input().split()))
+count = 0
+for i in range(n-2):
+    for j in range(i+1,n-1):
+        for k in range(j+1,n):
+            if(li[i]<li[j]<li[k]):
+                print(li[i],li[j],li[k])
+                count+=1
+print(count)
