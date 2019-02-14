3ff18d5710ec642e31fa99728ba728dd958a90af
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-02-14 16:10:32 +0530

    Create gcd_lis_queue.py

---
+++ gcd_lis_queue.py
@@ -0,0 +1,21 @@
+def divisible(li,k):
+    count = 0
+    for i in li:
+        if(i%k == 0):
+            count+=1
+    if(count == len(li)):
+        return True
+    else:
+        return False
+def factor(lis):
+    if(1 in lis):
+        return 1
+    else:
+        miner = min(lis)
+        for j in range(miner,0,-1):
+            if(divisible(lis,j)):
+                return j
+n,q = map(int,input().split())
+for i in range(q+1):
+    temp = list(map(int,input().split()))
+    print(factor(temp))
