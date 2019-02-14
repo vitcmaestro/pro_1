d761fc44cf802988887e9daedcadcfbd8612c322
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-02-14 22:54:58 +0530

    Update kolappam.py

--- kolappam.py
+++ kolappam.py
@@ -5,6 +5,5 @@ for i in range(n-2):
     for j in range(i+1,n-1):
         for k in range(j+1,n):
             if(li[i]<li[j]<li[k]):
-                print(li[i],li[j],li[k])
                 count+=1
 print(count)
