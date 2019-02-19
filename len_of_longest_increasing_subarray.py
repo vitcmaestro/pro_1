f8b99357b61c014c2f8eca861083032ac0bffcfa
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-02-19 14:49:21 +0530

    Create len_of_longest_increasing_subarray.py

---
+++ len_of_longest_increasing_subarray.py
@@ -0,0 +1,12 @@
+n = int(input())
+lis = list(map(int,input().split()))
+maxer = 0
+count = 1
+for i in range(1,n):
+    if(lis[i]>lis[i-1]):
+        count+=1
+        if(count> maxer):
+            maxer = count
+    else:
+        count = 0
+print(maxer)
