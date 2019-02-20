bba7a6f711f17b513a6ab338fce7b806f0ea24f6
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-02-20 23:25:19 +0530

    Create bin_sort_one_count.py

---
+++ bin_sort_one_count.py
@@ -0,0 +1,30 @@
+n = int(input())
+x = 1
+y = str(bin(x))
+ans = []
+fin =[]
+res =[]
+z = 1
+while(len(y) <= n+2):
+    ans.append(y[2:])
+    x+=1
+    y = str(bin(x))
+while(len(ans) >0 and z<=n):
+    i = 0
+    while(len(ans)>0 and i<len(ans)):
+        if(ans[i].count('1') == z):
+            fin.append(ans[i])
+            ans.remove(ans[i])
+        else:
+            i+=1
+    z+=1
+    res.append(fin)
+    fin =[]
+
+for i in res:
+    for j in i:
+        if(len(j) == 1):
+            j = "00"+j
+        if(len(j) == 2):
+            j = "0"+j
+        print(j)
